0f21321f90bc17922a662d0bb2040fd94f924bba
axelkirken <axel.kierkegaard@outlook.com>
2022-07-14 16:34:57 +0200

    made fewer functions

--- scriptwedding.js
+++ scriptwedding.js
@@ -27,168 +27,95 @@ const workflow = {
     state: 0,
 }
 
-//One card
-
-const moveRightOne = (obj) => {
+const moveLeft = (obj, num) => {
     let cardList = obj.cardList;
     let state = obj.state;
     let i = cardList.length;
     let oldCard = cardList[state];
-    let newCard = cardList[(state+i-1)%i];
     if (oldCard.style.animation !== "") return;
-    obj.state = (state + i-1) % i;
-    oldCard.style.animation = "moveRightOut ease-in-out " + animationSpeed;
-    newCard.style.animation = "moveRightIn ease-in-out " + animationSpeed;
-    newCard.classList.add("show"); 
+    oldCard.style.animation = `moveLeft${num}Out ease-in-out ${animationSpeed}`;
+    if (num === 2 || num === 3) {
+        var middleCard1 = cardList[(state+1)%i];
+        middleCard1.style.animation = `moveLeft${num}1 ease-in-out ${animationSpeed}`;
+    }
+    if (num === 3) {
+        var middleCard2 = cardList[(state+2)%i];
+        middleCard2.style.animation = `moveLeft${num}2 ease-in-out ${animationSpeed}`;
+    }
+    let newCard = cardList[(state+num)%i];
+    newCard.style.animation = `moveLeft${num}In ease-in-out ${animationSpeed}`;
+    newCard.classList.add(`show${num}`);
+    obj.state = (state + 1) % i;
     setTimeout(() => {
-        oldCard.classList.remove("show");
+        if (num === 2 || num ===3) {
+            middleCard1.classList.remove("show2");
+            middleCard1.classList.add("show1");
+            middleCard1.style.animation = "";
+        }
+        if (num ===3) {
+            middleCard2.classList.remove("show3");
+            middleCard2.classList.add("show2");
+            middleCard2.style.animation = "";
+        }        
+        oldCard.classList.remove("show1");
         oldCard.style.animation = "";
         newCard.style.animation = "";
-    }, speed-2);    
+    }, speed-2);  
 }
 
 
-const moveLeftOne = (obj) => {
+const moveRight = (obj, num) => {
+    console.log("test");
     let cardList = obj.cardList;
     let state = obj.state;
     let i = cardList.length;
-    let oldCard = cardList[state];
-    let newCard = cardList[(state+1)%i];
+    let oldCard = cardList[(state+num-1)%i];
     if (oldCard.style.animation !== "") return;
-    obj.state = (state + 1) % i;
-    oldCard.style.animation = "moveLeftOut ease-in-out " + animationSpeed
-    newCard.style.animation = "moveLeftIn ease-in-out " + animationSpeed
-    newCard.classList.add("show"); 
-    setTimeout(() => {
-        oldCard.classList.remove("show");
-        oldCard.style.animation = "";
-        newCard.style.animation = "";
-    }, speed-2);    
-}
-
-// Two cards
-
-const moveLeftTwo = (obj) => {
-    let cardList = obj.cardList;
-    let state = obj.state;
-    let i = cardList.length;
-    let oldCard = cardList[state];
-    let middleCard = cardList[(state+1)%i];
-    let newCard = cardList[(state+2)%i];
-    if (oldCard.style.animation !== "" || middleCard.style.animation !== "") return;
-    obj.state = (state + 1) % i;
-    oldCard.style.animation = "moveLeftTwoOut ease-in-out " + animationSpeed;
-    middleCard.style.animation = "moveLeftTwo ease-in-out " + animationSpeed;
-    newCard.style.animation = "moveLeftTwoIn ease-in-out " + animationSpeed;
-    newCard.classList.add("show2")
-    setTimeout(() => {
-        oldCard.classList.remove("show");
-        middleCard.classList.remove("show2");
-        middleCard.classList.add("show");
-        oldCard.style.animation = "";
-        middleCard.style.animation = "";
-        newCard.style.animation = "";
-    }, speed-2);  
-}
-
-
-const moveRightTwo = (obj) => {
-    let cardList = obj.cardList;
-    let state = obj.state;
-    let i = cardList.length;
-    let oldCard = cardList[(state+1)%i];
-    let middleCard = cardList[state];
+    oldCard.style.animation = `moveRight${num}Out ease-in-out ${animationSpeed}`;
+    if (num === 2 || num === 3) {
+        var middleCard1 = cardList[(state)%i];
+        middleCard1.style.animation = `moveRight${num}1 ease-in-out ${animationSpeed}`;
+    }
+    if (num === 3) {
+        var middleCard2 = cardList[(state+1)%i];
+        middleCard2.style.animation = `moveRight${num}2 ease-in-out ${animationSpeed}`;
+    }
     let newCard = cardList[(state+i-1)%i];
+    newCard.style.animation = `moveRight${num}In ease-in-out ${animationSpeed}`;
+    newCard.classList.add(`show1`);
     obj.state = (state + i-1) % i;
-    oldCard.style.animation = "moveRightTwoOut ease-in-out " + animationSpeed;
-    middleCard.style.animation = "moveRightTwo ease-in-out " + animationSpeed;
-    newCard.style.animation = "moveRightTwoIn ease-in-out " + animationSpeed;
-    newCard.classList.add("show");
     setTimeout(() => {
-        oldCard.classList.remove("show2");
-        middleCard.classList.remove("show");
-        middleCard.classList.add("show2");
+        if (num === 2 || num ===3) {
+            middleCard1.classList.remove("show1");
+            middleCard1.classList.add("show2");
+            middleCard1.style.animation = "";
+        }
+        if (num ===3) {
+            middleCard2.classList.remove("show2");
+            middleCard2.classList.add("show3");
+            middleCard2.style.animation = "";
+        }        
+        oldCard.classList.remove(`show${num}`);
         oldCard.style.animation = "";
-        middleCard.style.animation = "";
         newCard.style.animation = "";
     }, speed-2);  
+
 }
-
-
-// Three cards - only workflow carousel
-
-const moveLeftThree = () => {
-    let cardList = workflow.cardList;
-    let state = workflow.state;
-    let i = cardList.length;
-    let oldCard = cardList[state];
-    let middleCard1 = cardList[(state+1)%i];
-    let middleCard2 = cardList[(state+2)%i];
-    let newCard = cardList[(state+3)%i];
-    if (oldCard.style.animation !== "" || middleCard1.style.animation !== "") return;
-    workflow.state = (state + 1) % i;
-    oldCard.style.animation = "moveLeftThreeOut ease-in-out " + animationSpeed;
-    middleCard1.style.animation = "moveLeftThree1 ease-in-out " + animationSpeed;
-    middleCard2.style.animation = "moveLeftThree2 ease-in-out " + animationSpeed;
-    newCard.style.animation = "moveLeftThreeIn ease-in-out " + animationSpeed;
-    newCard.classList.add("show3");
-    setTimeout(() => {
-        middleCard1.classList.remove("show2");
-        middleCard1.classList.add("show");
-        middleCard2.classList.remove("show3");
-        middleCard2.classList.add("show2");
-        oldCard.classList.remove("show");
-        oldCard.style.animation = "";
-        middleCard1.style.animation = "";
-        middleCard2.style.animation = "";
-        newCard.style.animation = "";
-    }, speed-2);  
-}
-
-
-const moveRightThree = () => {
-    let cardList = workflow.cardList;
-    let state = workflow.state;
-    let i = cardList.length;
-    let oldCard = cardList[(state+2)%i];
-    let middleCard1 = cardList[(state)%i];
-    let middleCard2 = cardList[(state+1)%i];
-    let newCard = cardList[(state+i-1)%i];
-    if (oldCard.style.animation !== "" || middleCard1.style.animation !== "") return;
-    workflow.state = (state + i-1) % i;
-    oldCard.style.animation = "moveRightThreeOut ease-in-out " + animationSpeed;
-    middleCard1.style.animation = "moveRightThree1 ease-in-out " + animationSpeed;
-    middleCard2.style.animation = "moveRightThree2 ease-in-out " + animationSpeed;
-    newCard.style.animation = "moveRightThreeIn ease-in-out " + animationSpeed;
-    newCard.classList.add("show");
-    setTimeout(() => {
-        oldCard.classList.remove("show3");
-        middleCard1.classList.remove("show");
-        middleCard1.classList.add("show2");
-        middleCard2.classList.remove("show2");
-        middleCard2.classList.add("show3");
-        oldCard.style.animation = "";
-        middleCard1.style.animation = "";
-        middleCard2.style.animation = "";
-        newCard.style.animation = "";
-    }, speed-2);  
-}
-
 
 
 const moveLeftProduct = () => {
     if (window.matchMedia("(min-width: 768px)").matches) {
-        moveLeftTwo(products)
+        moveLeft(products, 2)
     } else {
-        moveLeftOne(products)
+        moveLeft(products, 1)
     }
 }
 
 const moveRightProduct = () => {
     if (window.matchMedia("(min-width: 768px)").matches) {
-        moveRightTwo(products)
+        moveRight(products, 2)
     } else {
-        moveRightOne(products)
+        moveRight(products, 1)
     }
 }
 
@@ -196,15 +123,15 @@ const moveRightProduct = () => {
 const moveLeftWorkflow = () => {
     if (window.matchMedia("(min-width: 992px").matches) {
         if (workflow.state === 1) return;
-        moveLeftThree()
+        moveLeft(workflow, 3)
         size="l"
     } else if (window.matchMedia("(min-width: 768px").matches) {
         if (workflow.state === 2) return;
-        moveLeftTwo(workflow)
+        moveLeft(workflow, 2)
         size="m"
     } else {
         if (workflow.state === 3) return;
-        moveLeftOne(workflow)
+        moveLeft(workflow, 1)
         size ="s"
     } 
     changeColor(size)
@@ -214,13 +141,13 @@ const moveRightWorkflow = () => {
     if (workflow.state === 0) return;
     let size = ""
     if (window.matchMedia("(min-width: 992px").matches){
-        moveRightThree();
+        moveRight(workflow, 3)
         size = "l";
     } else if (window.matchMedia("(min-width: 768px").matches) {
-        moveRightTwo(workflow)
+        moveRight(workflow , 2)
         size = "m"
     } else {
-        moveRightOne(workflow)
+        moveRight(workflow, 1)
         size = "s"
     } 
     changeColor(size)
@@ -258,29 +185,29 @@ const resize = () => {
     let size
     if (window.matchMedia("(min-width: 992px)").matches)  {
         if (workflow.state >= 2) {
-            workflow.cardList[workflow.state].classList.remove("show")
+            workflow.cardList[workflow.state].classList.remove("show1")
             workflow.cardList[(workflow.state+1)%4].classList.remove("show2")
             workflow.state = 1;
         }
-        workflow.cardList[workflow.state].classList.add("show")
+        workflow.cardList[workflow.state].classList.add("show1")
         workflow.cardList[(workflow.state+1)%4].classList.add("show2")
         workflow.cardList[(workflow.state+2)%4].classList.add("show3")
         size="l"
     } else if (window.matchMedia("(min-width: 768px)").matches) {
-        products.cardList[products.state].classList.add("show")
+        products.cardList[products.state].classList.add("show1")
         products.cardList[(products.state+1)%3].classList.add("show2")
         if (workflow.state === 3) {
-            workflow.cardList[workflow.state].classList.remove("show")
+            workflow.cardList[workflow.state].classList.remove("show1")
             workflow.state = 2;
         }
-        workflow.cardList[workflow.state].classList.add("show")
+        workflow.cardList[workflow.state].classList.add("show1")
         workflow.cardList[(workflow.state+1)%4].classList.add("show2")
         workflow.cardList[(workflow.state+2)%4].classList.remove("show3")
         size="m"
     } else {
-        products.cardList[products.state].classList.add("show")
+        products.cardList[products.state].classList.add("show1")
         products.cardList[(products.state+1)%3].classList.remove("show2")
-        workflow.cardList[workflow.state].classList.add("show")
+        workflow.cardList[workflow.state].classList.add("show1")
         workflow.cardList[(workflow.state+1)%4].classList.remove("show2")
         size="s"
     } 
